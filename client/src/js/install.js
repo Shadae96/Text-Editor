986164043e61850dc15f0880f90a80db96d830e0
Shadae96 <84427990+Shadae96@users.noreply.github.com>
2022-03-20 19:32:29 -0400

    install webpack

---
+++ client/src/js/install.js
@@ -0,0 +1,34 @@
+const butInstall = document.getElementById('buttonInstall');
+
+// Logic for installing the PWA
+// TODO: Add an event handler to the `beforeinstallprompt` event
+window.addEventListener('beforeinstallprompt', (event) => {
+    window.defferredPromp = event;
+
+    butInstall.classList.toggle('hidden', false);
+
+});
+
+// TODO: Implement a click event handler on the `butInstall` element
+butInstall.addEventListener('click', async () => {
+    const promptEvent = window.defferredPromp;
+
+    if (!promptEvent){
+        return;
+    }
+
+    promptEvent.prompt();
+
+    window.defferredPromp = null;
+
+    butInstall.classList.toggle('hidden', true);
+
+});
+
+// TODO: Add an handler for the `appinstalled` event
+window.addEventListener('appinstalled', (event) => {
+
+    window.defferredPromp = null;
+});
+
+
